8e704600fd195c97bed79b9b7243930f387166e1
Enevevet <moi.201214@hotmail.fr>
2020-12-26 11:41:23 +0100

    Ajout des eqnarray

--- index.js
+++ index.js
@@ -33,21 +33,24 @@ fs.readdir("./tex_files/", (err, files) => {
         par = par.replace(/ :/g, " :")
 
         //REPLACER DE MATHS
-        function replacer(match, p1, p2, p3, p4, offset, string) {
-            p2 = p2.replace(/\\overrightarrow{rot}/g, "\\rot ")
-            p2 = p2.replace(/\\overrightarrow{grad}/g, "\\grad ")
-            p2 = p2.replace(/div/g, "\\div")
-            p2 = p2.replace(/\(([^)]{5,}?)\)/g, "\\left($1\\right)")
-            p2 = p2.replace(/(?<!\\[a-z]*)\[/gi, "\\left[") //Genre c'est possible mdr
-            p2 = p2.replace(/(?<!\\[a-z]*\[[a-z]*)\]/gi, "\\right]") // JPP, pire arnaqueur du monde
-            p2 = p2.replace(/(?<!(\\o?i{1,3}nt|\\sum|\\product))_{(.[^}].*?)}/g, "_{\\mathrm{$2}}")
-            p2 = p2.replace(/(Espace)/gi, "\\mathrm{$1}")
-            p2 = p2.replace(/(Gauss)/gi, "\\mathrm{$1}")
-            p2 = p2.replace(/(?<!\\([a-z]|[A-Z])*)d/gm, "\\mathrm{d}") //MATHRM D PTN
-            p2 = p2.replace(/(?<!\\(([a-z]|[A-Z])*|(mathcal{)))D/gm, "\\mathrm{D}")
-            return `$${p2}$`;
+        function replacer(match, p1, p2, p3, p4, p5, offset, string) {
+            p3 = p3.replace(/\\overrightarrow{rot}/g, "\\rot ")
+            p3 = p3.replace(/\\overrightarrow{grad}/g, "\\grad ")
+            p3 = p3.replace(/div/g, "\\div")
+            p3 = p3.replace(/\(([^)]{5,}?)\)/g, "\\left($1\\right)")
+            p3 = p3.replace(/(?<!\\[a-z]*)\[/gi, "\\left[") //Genre c'est possible mdr
+            p3 = p3.replace(/(?<!\\[a-z]*\[[a-z]*)\]/gi, "\\right]") // JPP, pire arnaqueur du monde
+            p3 = p3.replace(/(?<!(\\o?i{1,3}nt|\\sum|\\product))_{(.[^}].*?)}/g, "_{\\mathrm{$2}}")
+            p3 = p3.replace(/(Espace)/gi, "\\mathrm{$1}")
+            p3 = p3.replace(/(Gauss)/gi, "\\mathrm{$1}")
+            p3 = p3.replace(/(?<!\\([a-z]|[A-Z])*)d/gm, "\\mathrm{d}") //MATHRM D PTN
+            p3 = p3.replace(/(?<!\\(([a-z]|[A-Z])*|(mathcal{)))D/gm, "\\mathrm{D}")
+            return `${p1}${p2}${p3}${p5}`;
         }
-        par = par.replace(/(\$)((.|\r?\n?)*?)(\$)/gm, replacer)
+
+        par = par.replace(/(\$)()((.|\r?\n?)*?)(\$)/gm, replacer)
+        par = par.replace(/(\\begin{eqnarray})(\\label{.*?})?((.|\r?\n?)*?)(\\end{eqnarray})/g, replacer)
+            //par = par.replace(/(\\mathcolorbox{.*?}{)()((.|\r?\n?)*?)(}\.( |\n?\r?))/g, replacer)
 
         par = par.replace(/<</g, "\\ll")
         par = par.replace(/>>/g, "\\gg")
